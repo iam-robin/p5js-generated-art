5657522b480ed461a61d4575792f83771da4f941
Robin Spielmann <spielmannrobin@gmail.com>
2020-12-02 18:57:08 +0100

    add line length algorithm

---
+++ 01_line-length.js
@@ -0,0 +1,39 @@
+let img;
+
+function preload() {
+    img = loadImage('assets/ryuichi-80.jpg');
+}
+
+function setup() {
+    createCanvas(600, 600, SVG);
+    print(img.width + ' • ' + img.height);
+}
+
+function draw() {
+    background(255);
+
+    for (let gridX = 0; gridX < img.width; gridX++) {
+        for (let gridY = 0; gridY < img.height; gridY++) {
+            // grid position + tile size
+            const tileWidth = width / img.width;
+            const tileHeight = height / img.height;
+            const posX = tileWidth * gridX;
+            const posY = tileHeight * gridY;
+
+            // get current color
+            img.loadPixels();
+            const c = color(img.get(gridX, gridY));
+
+            // greyscale conversion
+            const greyscale = round(red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071);
+
+            // greyscale to line length
+            let l3 = map(greyscale, 0, 255, 30, 0.1);
+            // l3 *= mouseXFactor;
+            l3 *= 0.3;
+            stroke(0);
+            strokeWeight(1);
+            line(posX, posY, posX + l3, posY + l3);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 001
+
+01 brightness to line length              |  02 test
+:-------------------------:|:-------------------------:
+![Agnes](_results/01/agnes.svg)  |  ![Ryuichi](_results/01/ryuichi.svg)
