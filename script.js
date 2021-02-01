7085a92dd2380384107f5f5082cfd8ded834ff62
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-02-01 18:50:30 +0100

    refactor: 💡 remove old code

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
+let shapes;
 let img;
-const IMAGE_SIZE_FACTOR = 5;
-const LINE_LENGTH_FACTOR = 0.6;
-const STROKE_WEIGHT = 1;
 
 function preload() {
-	img = loadImage('assets/agnes-80.jpg');
+	img = loadImage('assets/ryuichi-80.jpg');
 }
-
 function setup() {
-	const canvasWidth = IMAGE_SIZE_FACTOR * img.width;
-	const canvasHeight = IMAGE_SIZE_FACTOR * img.height;
-
-	createCanvas(canvasWidth, canvasHeight, SVG);
+	createCanvas(600, 600, SVG);
+	noStroke();
 	noLoop();
-	stroke(0);
-	strokeWeight(STROKE_WEIGHT);
+	fill('#000');
 	background(255);
-
-	print('image size: ' + img.width + ' x ' + img.height);
+	shapes = ['@', '%', '#', '*', '+', '=', '-', ':', '.', ' '];
+	// const letters = "@,$,B,%,8,&,W,M,#,*,o,a,h,k,b,d,p,q,w,m,Z,O,0,Q,L,C,J,U,Y,X,z,c,v,u,n,x,r,j,f,t,/,\,|,(,),1,{,},[,],?,-,_,+,~,<,>,i,!,l,I,;,:,^,`,',., ";
+	// shapes = letters.split(",")
 }
 
 function draw() {
-
+	
 	for (let gridX = 0; gridX < img.width; gridX++) {
 		for (let gridY = 0; gridY < img.height; gridY++) {
-			// grid position + tile size
-			const tileWidth = width / img.width;
-			const tileHeight = height / img.height;
-			const posX = tileWidth * gridX;
-			const posY = tileHeight * gridY;
+			// grid position + title size
+			const titleWidth = width / img.width;
+			const titleHeight = height / img.height;
+			const posX = titleWidth * gridX;
+			const posY = titleHeight * gridY;
 
 			// get current color
 			img.loadPixels();
-			const c = color(img.get(gridX, gridY));
+			const c = img.get(min(gridX, img.width - 1), gridY);
 
 			// greyscale conversion
 			const greyscale = round(red(c) * 0.222 + green(c) * 0.707 + blue(c) * 0.071);
-			// greyscale to line length
-			let lineLength = map(greyscale, 0, 255, 10, 0);
-
-			// adjust line lengths
-			lineLength *= LINE_LENGTH_FACTOR;
-			line(posX, posY, posX + lineLength, posY + lineLength);
+			const gradientToIndex = round(map(greyscale, 0, 255, 0, shapes.length - 1));
+			textSize(8);
+			text(shapes[gradientToIndex], posX, posY);
 		}
 	}
 }
