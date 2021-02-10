c4166a2434112ff56e192a791ecd9ea4913e99dd
Robin Spielmann <robin.spielmann@virtual-identity.com>
2021-02-10 19:08:14 +0100

    feat: 🎸 add noise line curtain algorithm

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -5,28 +5,30 @@ import { noise2D, getRandomSeed } from './helper.js';
 
 const sketch = (p) => {
 
-	const ITEMSIZE = 6;
-	const STROKEWIDTH = 0;
-	const COLUMNS = 40;
-	const ROWS = 60;
-	const PADDING = ITEMSIZE * 0.05;
+	const LINES = 60;
+	const LINE_SPACING = 4.5;
+	const STROKEWIDTH = 2;
+	const VERTICAL_STEPS = 8;
+	const VERTICAL_STEP_SPACING = 85;
 	const CANVAS_PADDING = 40;
-	const CANVAS_COLOR = '#44413F';
-	const SEED = getRandomSeed();
+	const CANVAS_COLOR = '#fff';
+	const NOISE_FACTOR = 50;
 
-	const GRIDBOX = ITEMSIZE + PADDING + STROKEWIDTH;
-	const START = GRIDBOX / 2;
+	const SEED = getRandomSeed();
+	const LINEWIDTH = STROKEWIDTH + LINE_SPACING;
+
 
 	p.setup = () => {
-		const totalX = GRIDBOX * COLUMNS;
-		const totalY = GRIDBOX * ROWS;
-		p.createCanvas(totalX + CANVAS_PADDING, totalY + CANVAS_PADDING, p.SVG);
+		const canvasWidth = LINEWIDTH * LINES + CANVAS_PADDING*2;
+		const canvasHeight = VERTICAL_STEPS * VERTICAL_STEP_SPACING - VERTICAL_STEP_SPACING + CANVAS_PADDING/2;
+		p.createCanvas(canvasWidth, canvasHeight, p.SVG);
 		
-		p.fill('#fff');
-		p.stroke(0);
+		p.noFill();
+		p.stroke('#000');
+		p.strokeWeight(STROKEWIDTH);
 		p.noLoop();
 		
-		console.log('SEED: ', SEED);  
+		console.log('SEED: ', SEED);
 		// add button events
 		document.querySelector('.export__svg').addEventListener('click', () => p.save(SEED + '.svg'));
 		document.querySelector('.export__png').addEventListener('click', () => p.save(SEED + 'canvas.png'));
@@ -34,29 +36,23 @@ const sketch = (p) => {
 		document.getElementById('defaultCanvas').style.padding = CANVAS_PADDING + 'px';
 		document.getElementById('defaultCanvas').style.backgroundColor = CANVAS_COLOR;
 	}
-
+	
 	p.draw = () => {
-		for (let x = 0; x < COLUMNS; x++) {
-			for (let y = 0; y < ROWS; y++) {
-				const u = x / (COLUMNS - 1);
-				const v = y / (ROWS - 1);
+		for (let x = 0; x <= LINES; x++) {
+			p.beginShape();
+			for (let y = 0; y <= VERTICAL_STEPS; y++) {
+				const u = x / (LINES - 1);
+				// const v = y / (VERTICAL_STEPS - 1);
+				const v = y / VERTICAL_STEP_SPACING;
 
-				const noiseSize = Math.abs(noise2D(u, v, 1, 1, SEED));
-				const size = ITEMSIZE/1.5 * noiseSize + ITEMSIZE/3;
+				const noise = Math.abs(noise2D(u, v, 1.4, 1.2, SEED) * NOISE_FACTOR);
 
-				const noiseTransform = Math.abs(noise2D(u, v, 1, 1, SEED)) * 30;
-
-				const centerX = START + x * GRIDBOX;
-				const centerY = START + y * GRIDBOX;
-
-				p.strokeWeight(STROKEWIDTH);
-				p.ellipse(
-					centerX + noiseTransform,
-					centerY + noiseTransform,
-					size,
-					size
-				);
+				const stepX = (x + 1) * LINEWIDTH;
+				const stepY = (y - 0.5) * VERTICAL_STEP_SPACING;
+				
+				p.curveVertex(stepX + noise, stepY + noise);
 			}
+			p.endShape();
 		}
 	}
 };
